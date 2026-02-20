1e58d874b2ac94cbe85245ef81cc469763257dd4
fadynaeem <fadynaeeem215@gmail.com>
2026-02-20 23:21:39 +0200

    init version

--- producer/src/main/java/com/example/notification/producer/ProducerApplication.java
+++
@@ -1,9 +0,0 @@
-package com.example.notification.producer;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootApplication
-public class ProducerApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(ProducerApplication.class, args);
-    }
-}
